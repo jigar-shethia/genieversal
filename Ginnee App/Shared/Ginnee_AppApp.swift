e86a2ef0cb003ba2f106fd430cdcb94f0bf76d27
jigar-shethia <30865218+jigar-shethia@users.noreply.github.com>
2022-01-02 02:28:49 +0530

    Add files via upload

---
+++ Ginnee App/Shared/Ginnee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ginnee_AppApp.swift
+//  Shared
+//
+//  Created by Jigar shethia on 02/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Ginnee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
